9d6b0474d7560ddf632f121baaf038533feb8d3e
JunkProvider <junkprovider@outlook.com>
2016-01-20 17:23:55 +0100

    Replaced some hard coded path seperators with DIRECTORY_SEPARATOR

--- source/server/autoload.php
+++ source/server/autoload.php
@@ -1,8 +1,8 @@
 <?php
 
 // The absolute paths to the class files.
-define('MODULES_PATH', 'source\\server\\modules\\');
-define('VENDOR_PATH', 'source\\server\\vendor\\');
+define('MODULES_PATH', 'source' . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR);
+define('VENDOR_PATH', 'source' . DIRECTORY_SEPARATOR . 'server' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR);
 
 /**
  * The autoload function. Loads class files from modules and vendor folder.
@@ -13,22 +13,24 @@ define('VENDOR_PATH', 'source\\server\\vendor\\');
  */
 function __autoload($className)
 {
+	$partialFilePath = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+
 	// Try loading the class from the modules folder.
-	$fileName = MODULES_PATH . $className . '.php';
-	if (file_exists($fileName))
+	$filePath = MODULES_PATH . $partialFilePath;
+	if (file_exists($filePath))
 	{
-		include $fileName;
+		include $filePath;
 		return;
 	}
 
 	// Try loading the class from vendor folder.
-	$fileName = VENDOR_PATH . $className . '.php';
-	if (file_exists($fileName))
+	$filePath = VENDOR_PATH . $partialFilePath;
+	if (file_exists($filePath))
 	{
-		include $fileName;
+		include $filePath;
 		return;
 	}
 
 	// The class file was not found in modules or vendor folder.
-	throw new \Exception('Could not load class. File "' . $fileName . '" does not exist.');
+	throw new \Exception('Could not load class. File "' . $partialFilePath . '" does not exist.');
 }
